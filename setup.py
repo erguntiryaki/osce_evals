f6045f40f7a974b718e471396e619c1d3ff8a17e
Ergün Tiryaki, MD <53856976+erguntiryaki@users.noreply.github.com>
2024-12-30 09:51:29 +0300

    dirs

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='osce_evals',
+    version='0.1.1',
+    packages=['osce'],
+    url='',
+    license='',
+    author='ergun tiryaki',
+    author_email='etiryaki16@ku.edu.tr',
+    description=''
+)
